3bb111061cd34d850b60389ec7353cc291d03a88
elmar.amanov <elmar.amanov@cybernet.az>
2023-08-30 10:48:13 +0400

    custom middlewares

--- 1-express/index.js
+++ 1-express/index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const playersRouter = require('./routers/playersRoutes');
+const logger = require('./express_middlewares/logger');
+const errorHandling = require('./express_middlewares/errorHandling');
 
 // create server
 const server = express();
 
 server.use(express.json());
+server.use(logger);
 
 // Requset Player Router we handle use with middleware
 server.use('/players', playersRouter);
+server.use(errorHandling);
 
 server.get('/', (req, res)=>{ res.send("Hello from express")});
 
--- 1-express/routers/playersRoutes.js
+++ 1-express/routers/playersRoutes.js
@@ -3,20 +3,14 @@ let db = require('../db/db');
 
 const router = Router();
 
+// get all players
 router.get('/', (req, res)=>{
   res.status(200).json(db);
 });
 
+// get player with id
 router.get('/:id', (req, res)=>{
   const { id } = req.params; // http://localhost:5000/1  id=1
-
-  const query = req.query; // http://localhost:5000/1?name:Messi&number=10  query = {name:'Messi, number: '10'};
-  console.log("QUERY IS :  ", query);
-
-  const body = req.body;
-  console.log("BODY IS: ", body);
-
-
   const actor = db.find(item=> item.id === parseInt(id));
   if(actor) res.status(200).json(actor);
   else res.status(404).send('Error 404');
@@ -24,12 +18,18 @@ router.get('/:id', (req, res)=>{
 
 // create player
 let player_ID = 3;
-router.post("/",(req, res)=>{
+router.post("/",(req, res, next)=>{
   const body = req.body;
-  const new_Player = {...body, player_ID};
-  player_ID++;
-  db.push(new_Player);
-  res.status(201).json(new_Player);
+  if(!body.name){
+    next({statusCode: 400, message: 'Bad Request enter the name'});
+  }else if(!body.group){
+    next({statusCode: 400, message: 'Bad Request enter the group'});
+  }else{
+    const new_Player = {...body, player_ID};
+    player_ID++;
+    db.push(new_Player);
+    res.status(201).json(new_Player);
+  };
 });
 
 // delete player
